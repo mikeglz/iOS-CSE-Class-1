ae6dc56d98c8b88cc7fee99a1887eaa69f0edc17
Linzhe Guo <lguo783@chil15.orchestra.cse.unsw.edu.au>
2011-03-29 15:50:14 +1100

    class1update

---
+++ Classes/person.h
@@ -0,0 +1,16 @@
+//
+//  person.h
+//  Class1
+//
+//  Created by Linzhe Guo on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface person : NSObject {
+
+}
+
+@end
---
+++ Classes/person.m
@@ -0,0 +1,14 @@
+//
+//  person.m
+//  Class1
+//
+//  Created by Linzhe Guo on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "person.h"
+
+
+@implementation person
+
+@end
